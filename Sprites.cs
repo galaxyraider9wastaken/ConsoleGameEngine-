1ff819c8205ebae61623dcad93bf16d7af759b80
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-27 13:40:50 -0700

    Update Sprites.cs

--- Sprites.cs
+++ Sprites.cs
@@ -3,7 +3,7 @@ using System;
 namespace ConsoleGameEngine{
   public class Sprite{
     public Vector2 Size;
-    public Vector2 File;
+    public string File;
     public Sprite(Vector2 size, string file){
       Size = size;
       File = file;
