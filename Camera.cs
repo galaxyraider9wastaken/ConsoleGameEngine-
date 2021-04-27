63876de66fa331883fa10cd8841d2a0f62a39a99
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-27 13:24:00 -0700

    Update Camera.cs

--- Camera.cs
+++ Camera.cs
@@ -4,9 +4,11 @@ namespace ConsoleGameEngine {
   public class Camera {
     public Vector2 Coordinates;
     public Vector2 Size;
+    public Matrix Cam;
     public Camera(Vector2 coordinates, Vector2 size) {
       Coordinates = coordinates;
       Size = size;
+      Cam = new Matrix(Size);
     }
   }
 }
