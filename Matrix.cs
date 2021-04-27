40e1b9ddabdbb6308f169ff79fddd106651cf067
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-27 13:21:26 -0700

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -1,1 +1,11 @@
+using System;
 
+namespace ConsoleGameEngine {
+  public class Matrix {
+    public Vector2 Size;
+    public float[][] M;
+    public Matrix(Vector2 size){
+      Size = size;
+    }
+  }
+}
