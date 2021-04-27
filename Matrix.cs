b06508a6680cc0dd9d4bd312b99c57f5a355f173
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-27 13:26:13 -0700

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -6,6 +6,10 @@ namespace ConsoleGameEngine {
     public float[][] M;
     public Matrix(Vector2 size){
       Size = size;
+      M = new float[size.x][];
+      for(int i=0;i<size.x;i++){
+        M[i] = new float[size.y];
+      }
     }
   }
 }
