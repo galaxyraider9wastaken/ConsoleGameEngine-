2d4f29eb883cac8e98639f3dc39e5a2e120df2d8
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-30 11:36:44 -0700

    Update Sprites.cs

--- Sprites.cs
+++ Sprites.cs
@@ -4,21 +4,19 @@ using System.IO;
 namespace ConsoleGameEngine{
   public class Sprite{
     public Vector2 Size;
-    public string File;
+    public string SFile;
     public Matrix CSprite;
     public Sprite(Vector2 size, string file){
       Size = size;
-      File = file;
-      CSprite = new string[Size.X][];
-      for(int i=0;i<size.X;i++){
-        CSprite[i] = new string[Size.Y];
-      }
+      SFile = file;
       var fx = File.ReadAllLines(file);
-      fx = new string[Size.Y][];
-      for(int i =0;i<Size.Y;i++){
-        fy[i] = fx[i].split();
+      var fy = new string[(int)Size.Y][];
+      var fym = new Matrix(Size);
+      for(int i =0;i<(int)Size.Y;i++){
+        fy[i] = fx[i].Split();
       }
-      CSprite = fy;
+      fym.M = fy;
+      CSprite = fym;
     }
   }
 }
