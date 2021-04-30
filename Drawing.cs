a39ccc583f0937bd8d868b22c9169722a0ee9719
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-30 11:38:32 -0700

    Update Drawing.cs

--- Drawing.cs
+++ Drawing.cs
@@ -1,8 +1,8 @@
 using System;
 
 namespace ConsoleGameEngine{
-  public class Console(){
-    public static void Draw(Vector2 Coordinates, Sprite Sprite){
+  public class Drawing{
+    public static void Draw_Sprite(Vector2 Coordinates, Sprite Sprite){
     
     }
   }
