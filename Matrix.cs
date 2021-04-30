ffd6b8076469cbe458cc270062ece1739a549d69
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-04-30 11:38:52 -0700

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -3,12 +3,12 @@ using System;
 namespace ConsoleGameEngine {
   public class Matrix {
     public Vector2 Size;
-    public float[][] M;
+    public string[][] M;
     public Matrix(Vector2 size){
       Size = size;
-      M = new string[size.x][];
-      for(int i=0;i<size.x;i++){
-        M[i] = new string[size.y];
+      M = new string[(int)size.X][];
+      for(int i=0;i<(int)size.X;i++){
+        M[i] = new string[(int)size.Y];
       }
     }
   }
