1e70aa1d05f884c945e06146089325daf90fb821
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-01 13:06:03 -0700

    Update Camera.cs

--- Camera.cs
+++ Camera.cs
@@ -5,10 +5,19 @@ namespace ConsoleGameEngine {
     public Vector2 Coordinates;
     public Vector2 Size;
     public Matrix Cam;
+    public string FillInChar = "=";
     public Camera(Vector2 coordinates, Vector2 size) {
       Coordinates = coordinates;
       Size = size;
       Cam = new Matrix(Size);
+      Clear();
+    }
+    public void Clear(){
+      for(int i =0;i<Size.X;i++){
+        for(int i1 =0;i1<Size.Y;i1++){
+          Cam.M[i][i1] = FillInChar;
+        }
+      }
     }
   }
 }
