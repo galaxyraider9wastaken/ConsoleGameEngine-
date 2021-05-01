fd18c72446e3ec213b2c7e8752f24e7b00ef9cd8
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-01 13:06:24 -0700

    Update Drawing.cs

--- Drawing.cs
+++ Drawing.cs
@@ -3,15 +3,24 @@ using System;
 namespace ConsoleGameEngine{
   public class Drawing{
     public static Camera DrawSprite(Vector2 DCoordinates, Sprite Sprite, Camera Camera){
+      var Iv = 0;
       var coords = new Vector2(Camera.Coordinates.X - DCoordinates.X, Camera.Coordinates.Y - DCoordinates.Y);
-      for(int i =0;i<Sprite.Size.X;i++){
-        for(int i1 =0;i1<Sprite.Size.Y;i1++){
-          Camera.Cam.M[(int)coords.X + i][(int)coords.Y + i1] = Sprite.CSprite.M[i][i1];
+      if(Iv == 0){
+        for(int i =0;i<Sprite.Size.X;i++){
+          if(coords.X + i <= Camera.Cam.M.Length && coords.X + i >= 0){
+            for(int i1 =0;i1<Sprite.Size.Y;i1++){
+              if(coords.Y + i1 <= Camera.Cam.M[i].Length && coords.Y + i1 >= 0){
+                Camera.Cam.M[(int)coords.X + i][(int)coords.Y + i1] = Sprite.CSprite.M[i][i1]; 
+              }
+            }
+          }
         }
       }
       return Camera;
     }
     public static void Draw(Camera Camera){
+      Console.SetCursorPosition(1,0);
+      Console.WriteLine("\n");
       for(int i = 0;i<(int)Camera.Size.X;i++){
         Console.WriteLine(string.Join("", Camera.Cam.M[i]));
       }
