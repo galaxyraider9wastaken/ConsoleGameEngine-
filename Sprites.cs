76facdad300e6a7eea4f38e1fdbbb0d712b1ae83
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-02 17:25:58 -0700

    Update Sprites.cs

--- Sprites.cs
+++ Sprites.cs
@@ -6,13 +6,24 @@ namespace ConsoleGameEngine{
     public Vector2 Size;
     public string SFile;
     public Matrix CSprite;
-    public Sprite(Vector2 size, string file){
+    public SpriteColor Colors;
+    public Sprite(Vector2 size, string file, SpriteColor colors){
       Size = size;
       SFile = file;
       CSprite = new Matrix(Size);
+      Colors = colors;
       var fx = File.ReadAllLines(file);
+      var fc = new char[(int)Size.X][];
       for(int i =0;i<(int)Size.X;i++){
-        CSprite.M[i] = fx[i].Split();
+        fc[i] = new char[(int)Size.Y];
+      }
+      for(int i =0;i<(int)Size.X;i++){
+        fc[i] = fx[i].ToCharArray();
+      }
+      for(int i =0;i<(int)Size.X;i++){
+        for(int i1 =0;i1<(int)Size.Y;i1++){
+          CSprite.M[i][i1] = fc[i][i1].ToString();
+        }
       }
     }
   }
