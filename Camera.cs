683418b09f792d03af4155a698b7b85fe8b58673
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-02 17:25:20 -0700

    Update Camera.cs

--- Camera.cs
+++ Camera.cs
@@ -5,17 +5,21 @@ namespace ConsoleGameEngine {
     public Vector2 Coordinates;
     public Vector2 Size;
     public Matrix Cam;
+    public Matrix CCam;
     public string FillInChar = "=";
+    public string WhiteSpaceChar = "~";
     public Camera(Vector2 coordinates, Vector2 size) {
       Coordinates = coordinates;
       Size = size;
       Cam = new Matrix(Size);
+      CCam = new Matrix(Size);
       Clear();
     }
     public void Clear(){
       for(int i =0;i<Size.X;i++){
         for(int i1 =0;i1<Size.Y;i1++){
           Cam.M[i][i1] = FillInChar;
+          CCam.M[i][i1] = "White";
         }
       }
     }
