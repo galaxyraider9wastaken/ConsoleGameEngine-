fb96dd08be0afa7d0c31f43136bffd063cac4e35
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-02 17:25:39 -0700

    Update Drawing.cs

--- Drawing.cs
+++ Drawing.cs
@@ -10,7 +10,10 @@ namespace ConsoleGameEngine{
           if(coords.X + i <= Camera.Cam.M.Length && coords.X + i >= 0){
             for(int i1 =0;i1<Sprite.Size.Y;i1++){
               if(coords.Y + i1 <= Camera.Cam.M[i].Length && coords.Y + i1 >= 0){
-                Camera.Cam.M[(int)coords.X + i][(int)coords.Y + i1] = Sprite.CSprite.M[i][i1]; 
+                if(Sprite.CSprite.M[i][i1] != Camera.WhiteSpaceChar){
+                  Camera.Cam.M[(int)coords.X + i][(int)coords.Y + i1] = Sprite.CSprite.M[i][i1]; 
+                  Camera.CCam.M[(int)coords.X + i][(int)coords.Y + i1] = Sprite.Colors.Colors.M[i][i1];
+                }
               }
             }
           }
@@ -21,8 +24,62 @@ namespace ConsoleGameEngine{
     public static void Draw(Camera Camera){
       Console.SetCursorPosition(1,0);
       Console.WriteLine("\n");
-      for(int i = 0;i<(int)Camera.Size.X;i++){
-        Console.WriteLine(string.Join("", Camera.Cam.M[i]));
+      for(int i =0;i<(int)Camera.Size.X;i++){
+        Console.WriteLine("");
+        for(int i1 =0;i1<(int)Camera.Size.Y;i1++){
+          switch(Camera.CCam.M[i][i1]){
+            case "Black":
+              Console.ForegroundColor = ConsoleColor.Black;
+              break;
+            case "DarkBlue":
+              Console.ForegroundColor = ConsoleColor.DarkBlue;
+              break;
+            case "DarkGreen":
+              Console.ForegroundColor = ConsoleColor.DarkGreen;
+              break;
+            case "DarkCyan":
+              Console.ForegroundColor = ConsoleColor.DarkCyan;
+              break;
+            case "DarkRed":
+              Console.ForegroundColor = ConsoleColor.DarkRed;
+              break;
+             case "DarkMagenta":
+              Console.ForegroundColor = ConsoleColor.DarkMagenta;
+              break;
+            case "DarkYellow":
+              Console.ForegroundColor = ConsoleColor.DarkYellow;
+              break;
+            case "Gray":
+              Console.ForegroundColor = ConsoleColor.Gray;
+              break;
+            case "DarkGray":
+              Console.ForegroundColor = ConsoleColor.DarkGray;
+              break;
+            case "Blue":
+              Console.ForegroundColor = ConsoleColor.Blue;
+              break;
+            case "Green":
+              Console.ForegroundColor = ConsoleColor.Green;
+              break;
+            case "Cyan":
+              Console.ForegroundColor = ConsoleColor.Cyan;
+              break;
+            case "Red":
+              Console.ForegroundColor = ConsoleColor.Red;
+              break;
+            case "Magenta":
+              Console.ForegroundColor = ConsoleColor.Magenta;
+              break;
+            case "Yellow":
+              Console.ForegroundColor = ConsoleColor.Yellow;
+              break;
+            case "White":
+              Console.ForegroundColor = ConsoleColor.White;
+              break;
+          }
+          Console.Write("" + Camera.Cam.M[i][i1]);
+          Console.ForegroundColor = ConsoleColor.White;
+        }
       }
     }
   }
